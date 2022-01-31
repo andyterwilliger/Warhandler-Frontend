63a99fda2b845f44dbf142d4d329a8c4d5894926
Andy Terwilliger <andy@terwilliger1@gmail.com>
2022-01-31 13:01:46 -0600

    getting up to date

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
+//Import CSS 
 import './css/App.css';
 import './css/Nav.css';
+
+//Import components
 import Nav from './components/Nav';
 import Main from './components/Main';
+import { Route, Switch } from 'react-router-dom';
+
+//Import Pages
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Main />
+
     </div>
   );
 }
