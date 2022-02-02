06e9079f09f8053a5efbe4e316438f9cef3b9170
Kendlyn Marriott <kendlynmarriott@Kendlyns-iMac.attlocal.net>
2022-02-02 15:02:02 -0500

    landing page added

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import MainCreate from './components/MainCreate';
 import Index from './pages/Index';
 
 
+
 function App() {
   const URL = 'https://warhandlerbackend.herokuapp.com/';
   return (
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,28 @@
+import WarHammerLogo from 'https://static.wikia.nocookie.net/warhammer40k/images/0/0f/Warhammer40000Logo.jpg/revision/latest/scale-to-width-down/2000?cb=20200406214317';
+import { Link } from 'react-router-dom'
+
+
+
+function LandingPage() {
+    return (
+        <div className='LandingPage'>
+        <img src={WarHammerLogo} alt='WarHammer Logo'/>
+        
+            <h1>THE DARK MILLENNIUM</h1>
+            <h2>FIGHT FOR THE FUTURE OF HUMANITY ACROSS A VICIOUS, WAR-TORN GALAXY</h2>
+            <p> Warhammer 40,000 puts you in command of an army of mighty warriors and war machines as you battle for supremacy in the grim darkness of the far future. 
+                In each game you deploy your forces against those of your opponent and strive to achieve victory in your chosen mission. 
+                Each of you will take it in turns to manoeuvre your units, fire upon the enemy lines and charge into combat, claiming contested ground and driving your foe from the battlefield. 
+                To emerge triumphant will require cunning, bravado and the luck of the dice, and through your conquests you can spread hope across the stars or set the galaxy aflame.
+                </p>
+
+            <h5>IN THE GRIM DARKNESS OF THE FAR FUTURE THERE IS ONLY WAR</h5>
+        <Link to='/'>
+        <button>Click to Enter</button>
+        </Link>
+       
+       
+        </div>
+    )
+}
+export default LandingPage;
